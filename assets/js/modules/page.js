17d94d85e18e01ac1fdee1ee0a1753da03c3c76a
Jogga Digital <operacoes@joggadigital.com.br>
2018-07-04 13:55:23 -0300

    Andando com serviços

--- assets/js/modules/page.js
+++ assets/js/modules/page.js
@@ -1,37 +1,65 @@
 var page = {
-	init: function(){
+  init: function() {
     this.geral();
+    this.slider();
   },
 
   geral: function() {
-
     // placeholder no ie
-    $('input, textarea').placeholder();
+    $("input, textarea").placeholder();
 
     // mascara telefone
-    var maskBehavior = function (val) {
-      return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-    },
-    options = {onKeyPress: function(val, e, field, options) {
-            field.mask(maskBehavior.apply({}, arguments), options);
+    var maskBehavior = function(val) {
+        return val.replace(/\D/g, "").length === 11
+          ? "(00) 00000-0000"
+          : "(00) 0000-00009";
+      },
+      options = {
+        onKeyPress: function(val, e, field, options) {
+          field.mask(maskBehavior.apply({}, arguments), options);
         }
-    };
+      };
 
-    $('.telefone').mask(maskBehavior, options);
+    $(".telefone").mask(maskBehavior, options);
 
     // validação
-    $('form').each(function() {  
-      $(this).validate({      
-        errorPlacement: function(error,element) {
+    $("form").each(function() {
+      $(this).validate({
+        errorPlacement: function(error, element) {
           return true;
         },
         submitHandler: function(form) {
-          $('form').find("button[type='submit']").prop('disabled',true);
-          $('form').submit();
+          $("form")
+            .find("button[type='submit']")
+            .prop("disabled", true);
+          $("form").submit();
         }
       });
     });
-    
+  },
+
+  slider: function() {
+    $(".services .owl-carousel").owlCarousel({
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          dots: true,
+          loop: true
+        },
+        600: {
+          items: 2,
+          dots: true,
+          loop: true
+        },
+        992: {
+          items: 3,
+          dots: false,
+          loop: false,
+          mouseDrag: false
+        }
+      }
+    });
   }
-}   
+};
 page.init();
